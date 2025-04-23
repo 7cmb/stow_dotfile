3f6c5ec74f5661328f7ff5bdf15bbbb0b79e4dde
7cmb <jiezhao.z@outlook.com>
2025-04-24 00:43:52 +0800

    change bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@ echo 'info: path to working directory is '"${work_dir}"
 #####
 function find_package(){
   [[ -n "$@" ]] || exit
-  for i in $(find -maxdepth 1 -type d -name '*' -not -path . -not -path ..);do
+  for i in $(find -maxdepth 1 -type d -name '*' -not -path ./.git -not -path . -not -path ..);do
     local prefix='./' 
     "$@" "${i##"$prefix"}"
   done
@@ -32,7 +32,7 @@ echo 'notification:' 'press "y" and enter to confirm that your packages is corre
 # Stow all the packages
 #####
 function stow_run(){
-  ([[ $# -eq 1 ]] && [[ $@ -eq y ]]) || (echo "ERROR: unavailable input, please input a \"y\" and match case" && exit)
+  ([[ $# -eq 1 ]] && [[ $@ == y ]]) || exec bash -c 'echo "ERROR: unavailable input, please input a \"y\" and match case" && exit'
   find_package "stow"
 }
 
@@ -49,7 +49,7 @@ input="n"
 echo 'notification:' 'press "y" to prepare compressive package <Cascadia Code Nerd Font> and <Source Code Pro Nerd Font>'
 read -p 'input: ' input
 bash -c 'echo $input;exit'
-if [[ "$input"="y" ]] && [[ -n "$input" ]];then
+if [[ "$input" == "y" ]] && [[ -n "$input" ]];then
   curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/CascadiaCode.zip
   curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/SourceCodePro.zip
   echo 'please copy the file you need to `/usr/loacal/share/fonts` '
